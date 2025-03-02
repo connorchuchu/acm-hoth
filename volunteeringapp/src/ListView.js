c3bb13358601a597c69de06d90c2ddb7e4e4b0d2
Brandon Lim <brandonlimgoog@gmail.com>
2025-03-02 14:41:30 -0800

    debugging

--- volunteeringapp/src/ListView.js
+++ volunteeringapp/src/ListView.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useNavigate } from "react-router-dom";
 import { useOpportunities } from "./OpportunityContext";
 
@@ -22,20 +21,6 @@ function ListView() {
             <p>Date: {opportunity.date}</p>
           </div>
         ))}
-=======
-function ListView({ opportunities, setOpportunities }) {
-    return (
-      <div style={{ padding: "20px", maxWidth: "400px", margin: "auto" }}>
-        <h2>Nearby Volunteering Opportunities</h2>
-        <div style={{ height: "300px", overflowY: "scroll", border: "1px solid #ccc", padding: "10px" }}>
-          {opportunities.map((opportunity) => (
-            <div key={opportunity.id} style={{ marginBottom: "10px", padding: "10px", border: "1px solid #ddd" }}>
-              <h3>{opportunity.title}</h3>
-              <p>Latitude: {opportunity.lat}, Longitude: {opportunity.lng}</p>
-            </div>
-          ))}
-        </div>
->>>>>>> parent of dcf8d0f (map and list stuff)
       </div>
     </div>
   );
